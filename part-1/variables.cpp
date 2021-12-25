75341ec3e2d0327954dd7ac2febf999e88647918
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-12-25 16:34:54 -0400

    🚀Changes: .vscode/settings.json part-1/variables.cpp

--- part-1/variables.cpp
+++ part-1/variables.cpp
@@ -7,12 +7,44 @@ Variables in C++ can alternatively be assigned an initial value when they are de
 
 #include <iostream>
 
+void showDataTypes(){
+  int integer = 5; // positive and negative, range depends on the compiler settings
+  short shortInteger = 45; // usually takes two bytes
+  long longInteger = - 9; // usually takes 4 bytes
+  unsigned int positiveInteger = 1;
+  unsigned short positiveShortInteger = 9;
+  unsigned long positiveLongInteger = 9000;
+
+  float floatingPoint = 5.3; // decimal
+  double doublePrecision = 5.33; // floating with more precision
+  char singleCharacter = 'a';
+  bool b = true; // true or false
+
+  // C++ doesn't provide a basic string type.
+  // But the standard implementation of string is provided by STD.
+  //std::cout<< type of value:
+  std::cout << positiveInteger;
+}
+
+void castingTypesExamples(){
+  /* Variables can be converted to other types by casting them.
+  * For example, an int can be cast to a bool.
+  * C++ provides three ways of explicitly changing the type of a variable. 
+  * The first method is a holdover from C, but is still the most commonly used. 
+  * The second method seems more natural at first but is rarely seen. 
+  * The third method is the most verbose, but often considered the cleanest.
+  */
+  int someInt = 2;
+  std::cout << someInt << std::endl;
+  bool someBool = (bool)someInt; // method 1
+  bool someBool2 = bool(someInt); // method 2
+  bool someBool3 = static_cast<bool>(someInt); // method 3
+  // It will be casted as a false if its zero, true otherwise
+  std::cout << someBool ;
+}
 using namespace std;
 int main(int argc, char** argv) {
-  int randomValue;
-  int initializedInt = 8;
-  cout << randomValue << " Is a random value \n";
-  cout << initializedInt << " Was assigned as initial value \n";
-  
+  //showDataTypes();
+  //castingTypesExamples();
   return 0;
 }
