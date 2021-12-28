d0266f6bbf4637530ad0850ebaec3c6b89486ab4
alejandro Lopez <alejandrolpz0619@gmail.com>
2021-12-28 11:56:43 -0400

    🚀 Changes: part-2/dinamicallyAllocatedArrays.cpp part-2/pointers.cpp

--- part-2/dinamicallyAllocatedArrays.cpp
+++ part-2/dinamicallyAllocatedArrays.cpp
@@ -7,19 +7,20 @@ int* allocateDinamicallyArray() {
 The * after the int type indicates that the variable you are declaring refers to some integer memory in the heap. Think of the pointer as an arrow that points at the dynamically allocated heap memory. It does not yet point to anything specific because you haven’t assigned it to anything; it is an uninitialized variable.
 */
 
-// To initialize the pointer to new heap memory, you use the new command:
-
   int arraySize = 5;
   int *myVariableSizedArray;
+  // To initialize the pointer to new heap memory, you use the new command
   myVariableSizedArray = new int[arraySize];
+  // adding elements to the array dinamically
   for (int i = 0; i < sizeof(myVariableSizedArray) -1; i++){
-    std::cout << i << std::endl;
     myVariableSizedArray[i] = i;
   }
+  // returning the pointer of the array
   return myVariableSizedArray;
 }
 int main() {
   int *result = allocateDinamicallyArray();
+  // looping through it.
   for (int i = 0; i < sizeof(result) - 1; i++) {
     std::cout << result[i] << std::endl;
   }
---
+++ part-2/pointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// We can put a variable in the heap by using a similar syntax:
+int main(){
+  // declaring a variable in the heap with the value of 5
+  int * myIntegerPointer = new int[5];
+  // "shadowing the variable"
+  *myIntegerPointer = 6;
+
+  // Pointers don’t always point to heap memory. You can declare a pointer that points to a variable on the stack, even another pointer. 
+  //To get a pointer to a variable, you use the & “address of” operator:
+  int i = 55;
+  // This variable is equal to the value pointed by i, which is 55
+  int *myIntegerPointer2 = &i; // Points to the variable with the value of 55
+
+  /*
+  C++ has a special syntax for dealing with pointers to structures. Technically, if you have a pointer to a structure, you can access its fields by first dereferencing it with *, then using the normal. syntax, as in the code that follows, which assumes the existence of a function called getEmployee().
+
+  EmployeeT* anEmployee = getEmployee();
+  cout << (*anEmployee).salary << endl;
+
+  This syntax is a little messy. The -> (arrow) operator lets you perform both the dereference and the field access in one step. The following code is equivalent to the preceding code but is easier to read.
+
+  EmployeeT* anEmployee = getEmployee();
+  cout << anEmployee->salary << endl;
+  */
+ 
+  
+}
+
